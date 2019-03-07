4894fbde5515b7b3604eefff5a29e2e5ff495f3c
Daniel <daniel.elliott.lewis@gmail.com>
2019-03-07 13:38:59 +1100

    requirements and runtime

--- run.py
+++ run.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
 
 # Alternatively you can set environment variables as:
 # set FLASK_APP = your_init_file.py
-# set FLASK_DEBUT = 1
+# set FLASK_DEBUG = 1
 # flask run
 # This allows us to use the flask shell for debugging puporposes
