d5f7fe99bcd3bb70286b80a2c086358e15f635aa
Daniel <daniel.elliott.lewis@gmail.com>
2019-03-07 14:08:46 +1100

    Added variable port to support Heroku env variable

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flaskblog import create_app
+import os
 
 app = create_app()
 
 # If you don't want to use environment variables you can do this:
 if __name__ == "__main__":
-    app.run(debug=True)
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
 
 # Alternatively you can set environment variables as:
 # set FLASK_APP = your_init_file.py
